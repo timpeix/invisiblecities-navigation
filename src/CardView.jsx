0dd5a42755b5fe01b7d9f37fb8e18b53781ce87b
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-04-16 11:52:36 +0200

    Fix focus problem but calculating target manually

--- src/CardView.jsx
+++ src/CardView.jsx
@@ -36,7 +36,7 @@ var CardItem = React.createClass({
       zIndex: this.state.zIndex
     };
     return (
-      <div className={classes} style={divStyle} onClick={this.focus}>
+      <div className={classes} style={divStyle}>
         <img src={this.props.image}/>
         <div className='block'>
           <div className='author' onClick={this.goToProject}>{this.props.author}</div>
@@ -51,15 +51,6 @@ var CardItem = React.createClass({
     var p = this.props;
     window.location.href = `thepony://o/${p.path}?landscape=${p.landscape}&specialRotate=${p.specialRotate}`;
   },
-  focus: function(e) {
-    console.log(87, this.props.author, e);
-    if (!this.state.focus) {
-      this.props.parent.removeAllFocus(this);
-    } else {
-      this.props.parent.removeAllFocus();
-    }
-    this.props.parent.setTransitioning(true);
-  }
 });
 
 
@@ -67,7 +58,8 @@ var CardView = React.createClass({
   getInitialState: function() {
     return {
       focus: false,
-      transitioning: false
+      transitioning: false,
+      focusItem: 0
     };
   },
   render: function() {
@@ -87,32 +79,88 @@ var CardView = React.createClass({
     
     this.cardItems = cardNodes;
     
-    return (<div className={classes}>
+    return (<div className={classes} onClick={this.focus}>
         <div className={numClass}>
           {cardNodes}
         </div>
       </div>
     );
   },
-  removeAllFocus: function(except) {
+  getCorrectTarget: function (x, y) {
+    if (this.state.transitioning) {
+      console.log("cant handle clicks while animating");
+      return false;
+    }
+    
+    var target = 0;
+    var scrollPos = this.getDOMNode().scrollTop;
+    var targetY = scrollPos + y;
+    
+    var heights = {
+      small: ~~(window.innerHeight * 0.08),
+      medium:  ~~(window.innerHeight * 0.3),
+      large: ~~(window.innerHeight * 0.6)
+    }
+    
+    console.log('current focus', this.state.focusItem);
+    
+    if (this.state.focus) {
+      var curY = 0;
+      var i = -1;
+      while(curY <= targetY) {
+        i++;
+        curY += (this.state.focusItem == i) ? heights.large : heights.small;
+      }
+      target = i;
+    } else {
+      target = Math.floor(targetY / heights.medium)
+    }
+    console.log('tapped number', target);
+    return target;
+  },
+  
+  focus: function(e) {
+    var target = this.getCorrectTarget(e.clientX, e.clientY);
+    
+    if (this.state.focus && target == this.state.focusItem) {
+      this.removeAllFocus(false);
+    } else {
+      this.removeAllFocus(true, target);
+    }
+    
+    this.setTransitioning(true);
+  },
+  
+  removeAllFocus: function(refocus, newItem) {
+    console.log('refocus:', refocus, 'newly focussed:', newItem);
     var found = false;
-    var at = i;
+    var at = 0;
+    // Loop through all children
     for (var i in this.refs) {
-      var isExcept = (except == this.refs[i]);
+      var focusThis = (newItem === parseInt(i));
+      
       this.refs[i].setState({
-        before: (!found && !isExcept && except),
-        after: (found && except),
-        focus: isExcept
+        before: (!found && !focusThis && refocus),
+        after: (found && refocus),
+        focus: focusThis
       });
-      if (isExcept) {
+      
+      console.log(focusThis, i, this.refs[i].state);
+      
+      if (focusThis) {
         found = true;
-        at = i;
+        at = parseInt(i);
       };
     }
+     
     this.setState({
-      focus: !!(except)
+      focus: refocus,
+      focusItem: at
     })
-    //this.getDOMNode().scrollTop = 0;
+    
+    console.log('new focus item', at);
+    console.log(this.state);
+    
   },
   setTransitioning: function (bool) {
     this.setState({
